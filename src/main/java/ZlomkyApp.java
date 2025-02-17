3567fbe4b19fc9da6db41e575e1f3157317fc859
honiger <honigka1@uhk.cz>
2025-02-17 10:50:02 +0100

    Added zlomek immutable (Read only) POJO class

--- src/main/java/ZlomkyApp.java
+++ src/main/java/ZlomkyApp.java
@@ -1,6 +1,15 @@
+import cz.uhk.zlomky.model.Zlomek;
+
 public class ZlomkyApp {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args)
+    {
+        System.out.println("Zlomkoaplikace");
+
+        // vytvoření instance zlomku třídy Zlomek
+
+        Zlomek a = new Zlomek(3,4);
+
+        System.out.printf("Zlomek a = %s\n", a);
     }
 }
