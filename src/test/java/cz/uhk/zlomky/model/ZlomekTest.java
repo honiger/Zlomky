91bb5775c85faeafbbc357302033e028db9b0413
honiger <honigka1@uhk.cz>
2025-03-03 10:00:16 +0100

    Dokončení testů

--- src/test/java/cz/uhk/zlomky/model/ZlomekTest.java
+++ src/test/java/cz/uhk/zlomky/model/ZlomekTest.java
@@ -26,17 +26,87 @@ class ZlomekTest {
 
     @Test
     void deleno() {
+        Zlomek a = new Zlomek(3, 4);
+        Zlomek b = new Zlomek(4, 5);
+        Zlomek c = a.deleno(b);
+
+        assertEquals(15, c.getCitatel(), "Chyba");
+        assertEquals(16, c.getjmenovatel(), "Chyba");
+    }
+
+    @Test
+    void deleniNulou() {
+        Zlomek a = new Zlomek(3, 4);
+        Zlomek b = new Zlomek(0, 5);
+
+        assertThrows(ArithmeticException.class, () -> a.deleno(b), "Mělo vyhodit výjimku!");
     }
 
     @Test
     void plus() {
+        Zlomek a = new Zlomek(3, 4);
+        Zlomek b = new Zlomek(4, 5);
+        Zlomek c = a.plus(b);
+
+        assertEquals(31, c.getCitatel(), "Chyba");
+        assertEquals(20, c.getjmenovatel(), "Chyba");
+    }
+
+    @Test
+    void scitaniSNulou() {
+        Zlomek a = new Zlomek(3, 4);
+        Zlomek b = new Zlomek(0, 5);
+        Zlomek c = a.plus(b);
+
+        assertEquals(3, c.getCitatel(), "Chyba");
+        assertEquals(4, c.getjmenovatel(), "Chyba");
     }
 
     @Test
     void minus() {
+        Zlomek a = new Zlomek(3, 4);
+        Zlomek b = new Zlomek(4, 5);
+        Zlomek c = a.minus(b);
+
+        assertEquals(3*5 - 4*4, c.getCitatel(), "Chyba");
+        assertEquals(20, c.getjmenovatel(), "Chyba");
+    }
+
+    @Test
+    void odcitaniSNulou() {
+        Zlomek a = new Zlomek( 3, 4);
+        Zlomek b = new Zlomek( 0, 5);
+        Zlomek c = a.minus(b);
+
+        assertEquals(3, c.getCitatel(), "Chyba");
+        assertEquals(4, c.getjmenovatel(), "Chyba");
     }
 
     @Test
     void zkratit() {
+        Zlomek a = new Zlomek( 8, 12);
+        Zlomek c = a.zkratit();
+
+        assertEquals(2, c.getCitatel(), "Chyba");
+        assertEquals(3, c.getjmenovatel(), "Chyba");
+    }
+
+
+    @Test
+    void kraceniJizZkraceneho() {
+        Zlomek a = new Zlomek( 3, 4);
+        Zlomek c = a.zkratit();
+
+        assertEquals(3, c.getCitatel(), "Chyba");
+        assertEquals(4, c.getjmenovatel(), "Chyba");
+    }
+
+    @Test
+    void kraceniJednickovehoZlomku() {
+        Zlomek a = new Zlomek( 5, 5);
+        Zlomek c = a.zkratit();
+
+        assertEquals(1, c.getCitatel(), "Chyba");
+        assertEquals(1, c.getjmenovatel(), "Chyba");
     }
 }
